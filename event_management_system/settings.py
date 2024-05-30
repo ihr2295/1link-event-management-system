a0c75b8b81c6eb03780bea9008ed47e10fa5bb0d
ihr2295 <134147685+ihr2295@users.noreply.github.com>
2024-05-30 14:29:36 +0200

    updating setting

--- event_management_system/settings.py
+++ event_management_system/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'event_management_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.config(conn_max_age=600)
 }
 
 # Password validation

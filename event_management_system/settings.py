fe410a3d019ce0bcf642c1f1a31cbb84a8928ff3
ihr2295 <134147685+ihr2295@users.noreply.github.com>
2024-05-30 14:07:09 +0200

    updating setting to resolve 500 server error

--- event_management_system/settings.py
+++ event_management_system/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'your-default-secret-key')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'DJANGO_DEBUG')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['event-management-system-3014bff26c17.herokuapp.com', '127.0.0.1', 'localhost']
 
 # settings.py
 LOGIN_REDIRECT_URL = '/'

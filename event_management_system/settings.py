0cb28faab567caa13a716649971ba9203c89897a
ihr2295 <134147685+ihr2295@users.noreply.github.com>
2024-06-02 14:12:48 +0200

    Solving Deployment and updating setting

--- event_management_system/settings.py
+++ event_management_system/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
-import django_heroku
+import django_heroku # type: ignore
 django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.

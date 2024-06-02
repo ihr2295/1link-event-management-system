7c3c66c4bfe55640f38b631d50ca212e063fbe99
ihr2295 <134147685+ihr2295@users.noreply.github.com>
2024-06-02 15:34:18 +0200

    removing extra requriments

--- event_management_system/settings.py
+++ event_management_system/settings.py
@@ -14,7 +14,7 @@ import os
 from pathlib import Path
 import dj_database_url
 import django_heroku
-django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,3 +123,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())

59333562d9aec5748b56ac019dee8aa73d061781
ihr2295 <134147685+ihr2295@users.noreply.github.com>
2024-06-04 23:05:30 +0200

    Add EventForm to forms.py

--- events/forms.py
+++ events/forms.py
@@ -3,6 +3,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Event
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -10,3 +11,8 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+class EventForm(forms.ModelForm):
+    class Meta:
+        model = Event
+        fields = ['title', 'description', 'date', 'location']
